c866e93a454d4828553036f03f70b216f5d1e2d9
JeffreyIsm <hypernexusxd@gmail.com>
2025-04-09 20:21:36 +0800

    purple

--- rooms_app/forms.py
+++ rooms_app/forms.py
@@ -5,6 +5,14 @@ class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
         fields = ['orientation', 'window_size', 'furniture', 'notes', 'status']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Exclude 'occupiedpark' from the status dropdown
+        self.fields['status'].choices = [
+            choice for choice in self.fields['status'].choices if choice[0] != 'occupiedpark'
+        ]
 
 class TenantForm(forms.ModelForm):
     class Meta:
